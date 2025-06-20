68cd2be47d81d72740d38d06ca8150270f42159d
Barkotfrew <141666169+Barkotfrew@users.noreply.github.com>
2025-06-20 17:56:19 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-const num = parseInt(arg);
 
-if (isNaN(num) || num <= 0) {
-  console.log('Missing number of occurrences');
-} else {
-  let i = 0;
-  while (i < num) {
-    console.log('C is fun');
-    i++;
+if (arg !== undefined) {
+  const x = parseInt(arg);
+  if (!isNaN(x)) {
+    for (let i = 0; i < x; i++) {
+      console.log('C is fun');
+    }
+  } else {
+    console.log('Missing number of occurrences');
   }
 }
